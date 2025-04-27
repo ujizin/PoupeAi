e1fd22ac6d5e41a2530cb09aa4263b043aaf5efb
ujizin <lucasyujideveloper@gmail.com>
2025-04-27 23:52:44 +0100

    build: add kotlinx serialization

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinxSerialization)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
