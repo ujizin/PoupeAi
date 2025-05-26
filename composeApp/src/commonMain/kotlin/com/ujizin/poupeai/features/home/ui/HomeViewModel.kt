2078381a05e77fe1da9edbec88861e90e6abc4d0
ujizin <lucasyujideveloper@gmail.com>
2025-05-26 22:52:53 +0100

    feat: add repository to viewmodel

--- composeApp/src/commonMain/kotlin/com/ujizin/poupeai/features/home/ui/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/ujizin/poupeai/features/home/ui/HomeViewModel.kt
@@ -1,7 +1,14 @@
 package com.ujizin.poupeai.features.home.ui
 
 import androidx.lifecycle.ViewModel
+import com.ujizin.poupeai.domain.repository.CategoryRepository
+import com.ujizin.poupeai.domain.repository.ExpenseRepository
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
-class HomeViewModel : ViewModel()
+class HomeViewModel(
+    private val expenseRepository: ExpenseRepository,
+    private val categoryRepository: CategoryRepository,
+) : ViewModel() {
+
+}
