b351a1e53971f66d0c9a1931ff0e61ae8456f15f
ujizin <lucasyujideveloper@gmail.com>
2025-06-21 19:35:19 +0100

    feat: add settings screen route

--- composeApp/src/commonMain/kotlin/com/ujizin/poupeai/features/settings/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ujizin/poupeai/features/settings/ui/SettingsScreen.kt
@@ -2,9 +2,12 @@ package com.ujizin.poupeai.features.settings.ui
 
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun SettingsRoute() {
+fun SettingsRoute(
+    viewModel: SettingsViewModel = koinViewModel()
+) {
 
     SettingsScreen()
 }
---
+++ composeApp/src/commonMain/kotlin/com/ujizin/poupeai/features/settings/ui/SettingsViewModel.kt
@@ -0,0 +1,9 @@
+package com.ujizin.poupeai.features.settings.ui
+
+import androidx.lifecycle.ViewModel
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+class SettingsViewModel : ViewModel() {
+
+}
